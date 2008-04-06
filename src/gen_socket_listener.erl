26869ad41c28b78ae0c1f6bf90c2e6697cf61e11
Dave Peticolas <dave@krondo.com>
2008-04-06 15:57:01 -0700

    Rename. Update docs.

--- src/gen_socket_listener.erl
+++ src/gen_socket_listener.erl
@@ -1,39 +1,12 @@
-%%%------------------------------------------------------------------------
-%%% File: $Id$
-%%%------------------------------------------------------------------------
-%%% @doc Generic tcp_listener.  See:
+%%%-------------------------------------------------------------------
+%%% @author Serge Aleynikov <saleyn@gmail.com>
+%%% @copyright (C) 2007, 2008, Serge Aleynikov
+%%% @doc Generic socket listener.  See:
 %%%      [http://www.trapexit.org/index.php/Building_a_Non-blocking_TCP_server_using_OTP_principles]
-%%%
-%%% @author  Serge Aleynikov <saleyn@gmail.com>
-%%% @version $Rev$
-%%%          $Date$
 %%% @end
-%%%------------------------------------------------------------------------
 %%% Created 2007-07-14
-%%%------------------------------------------------------------------------
-%%% Copyright (c) 2007 Serge Aleynikov
-%%%
-%%% Permission is hereby granted, free of charge, to any person obtaining
-%%% a copy of this software and associated documentation files
-%%% (the "Software"), to deal in the Software without restriction,
-%%% including without limitation the rights to use, copy,
-%%% modify, merge, publish, distribute, sublicense, and/or sell copies of
-%%% the Software, and to permit persons to whom the Software is furnished
-%%% to do so, subject to the following conditions:
-%%%
-%%% The above copyright notice and this permission notice shall be
-%%% included in all copies or substantial portions of the Software.
-%%%
-%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-%%% BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-%%% ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-%%% CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-%%% THE SOFTWARE.
-%%%------------------------------------------------------------------------
--module(tcp_listener).
+%%%-------------------------------------------------------------------
+-module(gen_socket_listener).
 -author('saleyn@gmail.com').
 
 -behaviour(gen_server).
@@ -175,7 +148,7 @@ handle_info({inet_async, ListSock, Ref, {ok, CliSocket}},
     ok ->
         %% New client connected - spawn a new process using the simple_one_for_one
         %% supervisor.
-        {ok, Pid} = tcp_listener_sup:start_client(SupName),
+        {ok, Pid} = gen_socket_listener_sup:start_client(SupName),
         gen_tcp:controlling_process(CliSocket, Pid),
         %% Instruct the new FSM that it owns the socket.
         Module:set_socket(Pid, CliSocket),
