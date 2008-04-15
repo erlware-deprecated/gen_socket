58b33fffe914d9cd1fad2a7e7a5eaa4fa09448d6
Dave Peticolas <dave@krondo.com>
2008-04-14 20:06:21 -0700

    Add @spec lines and more documentation.

--- examples/echo.erl
+++ examples/echo.erl
@@ -19,22 +19,36 @@
 -export([start_demo/0, start_link/0, set_socket/2]).
 
 
-% Start the echo server demo on port 9999.
+% Start the echo server demo on port 9999. This returns {ok, Pid}
+% where Pid is the socket listener supervisor. The gen_socket server
+% is implemented with two supervisors, one to manage the server which
+% accepts new connections on the listening socket, and one to manage
+% and create servers to handle each new connection.
+%
+% The servers which manage the connection are implemented by client
+% code. This module implements a line-based echo server.
+%
 % In a real application, The socket listener supervisor
 % would be started by a higher-level supervisor with a
 % spec created by gen_socket_listener_sup:get_supervisor_spec.
+%
+% @spec start_demo() -> {ok, Pid}
 start_demo() ->
     gen_socket_listener_sup:start_link(echo, "echo", 9999, echo, []).
 
 % This is called by the connection supervisor when the
 % gen_socket_listener_sup:start_client function is called.
 % It starts a new server for a single connection.
+%
+% @spec start_link() -> {ok, Pid}
 start_link() ->
     {ok, spawn_link(fun server/0)}.
 
 % This is called by the gen_socket_listener process to
 % pass the socket that a new connection process will be
 % responsible for.
+%
+% @set_socket(Pid, Socket) -> void
 set_socket(Pid, Socket) ->
     Pid! {socket_ready, Socket}.
 
