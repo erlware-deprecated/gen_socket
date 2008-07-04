fdbc1c369a8ae6358ae6734a9af3f1c035e2d367
Dave Peticolas <dave@krondo.com>
2008-07-03 22:16:29 -0700

    Require ok result from controlling_process.

--- src/gen_socket_listener.erl
+++ src/gen_socket_listener.erl
@@ -175,7 +175,7 @@ handle_info({inet_async, ListSock, Ref, {ok, CliSocket}},
         %% New client connected - spawn a new process using the simple_one_for_one
         %% supervisor.
         {ok, Pid} = gen_socket_listener_sup:start_client(SupName),
-        gen_tcp:controlling_process(CliSocket, Pid),
+        ok = gen_tcp:controlling_process(CliSocket, Pid),
         %% Instruct the new process that it owns the socket.
         Module:set_socket(Pid, CliSocket),
         {ok, NewRef} = prim_inet:async_accept(ListSock, -1),
